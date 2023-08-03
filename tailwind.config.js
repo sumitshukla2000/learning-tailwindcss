aada8ae8f42330a3c292c5311e3416fbd69eaf72
sumitshukla2000 <shuklasumit93@gmail.com>
2023-08-03 11:50:05 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        body: ["Borel"]
+      }
+    },
+  },
+  plugins: [],
+}
