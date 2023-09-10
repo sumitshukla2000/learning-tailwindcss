f5c23162c1930c8f1e093150203c69355ed97a4e
sumitshukla2000 <shuklasumit93@gmail.com>
2023-09-10 17:06:46 +0530

    added burger button fot responsiveness

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu");
+const burger = document.getElementById("burger");
+
+
+burger.addEventListener("click" , ()=>{
+    menu.classList.toggle("hidden");
+})
